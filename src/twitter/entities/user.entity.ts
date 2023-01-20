47404e68bd5b79e10952f439ed517ce563b28255
diego-wendt <diego_wendt@hotmail.com>
2023-01-20 02:22:48 -0300

    Ex1

--- src/twitter/entities/user.entity.ts
+++ src/twitter/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { TweetEntity } from './tweet.entity';
 
 @Entity({ name: 'User' })
 export class UserEntity {
@@ -16,4 +17,7 @@ export class UserEntity {
 
   @Column()
   photoUrl: string;
+
+  @OneToMany(() => TweetEntity, (tweet) => tweet.user)
+  tweets: TweetEntity[];
 }
